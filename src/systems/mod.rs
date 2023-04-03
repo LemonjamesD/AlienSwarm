9de50a2e66ac3d51d2c2efebb2993947152ab6da
LemonjamesD <lemon@lemonjamesd.com>
2023-04-03 18:03:39 -0400

    Added Thing for wrapping
    Signed-off-by: LemonjamesD <lemon@lemonjamesd.com>

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,3 +2,12 @@ pub mod blocks;
 
 pub mod health_component;
 pub mod human;
+
+use crate::systems::blocks::{air::AirBlock, dirt::DirtBlock};
+use crate::systems::human::Human;
+
+pub enum Thing {
+    Human(Human),
+    Dirt(DirtBlock),
+    Air(AirBlock)
+}
