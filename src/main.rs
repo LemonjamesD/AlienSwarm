fbde953fe7d4e037764cede42ab29145348bc73c
LemonjamesD <lemon@lemonjamesd.com>
2023-04-08 20:49:19 -0400

    Added tracing for help with logging
    Signed-off-by: LemonjamesD <lemon@lemonjamesd.com>

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use crossterm::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let file_appender = tracing_appender::rolling::hourly("./", "game.logs");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .init();
+    
     let mut game = Game::new()?;
 
     game.game_loop().await?;
